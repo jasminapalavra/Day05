867774e27567d788baca2626c320fc0f2033a26c
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-12 22:36:06 +0100

    Added ex08 src and index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line 
+    var lastName = "Doe"
+    var lastNameLength = 3;
+
+    "Doe".length; 
+    
+    // Only change code above this line 
+    return lastNameLength;
+        
+}
+console.log(main());
+module.exports = main;
